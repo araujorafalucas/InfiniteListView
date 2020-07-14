cff08f7750391a4480a1044f2523b66d1f74eeb9
Lucas Araújo <lucas.rafael.araujo@hotmail.com>
2020-07-13 21:21:26 -0300

    Listview implemented

---
+++ InfiniteListView/InfiniteListView/Custom/ListViewInfiniteScroll.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace InfiniteListView.Custom
+{
+    public class ListViewInfiniteScroll : ListView
+    {
+        public static BindableProperty CanLoadMoreProperty = BindableProperty.Create(
+                       "CanLoadMore",
+                       typeof(bool),
+                       typeof(ListView), false);
+
+        public bool CanLoadMore
+        {
+            get
+            {
+                return (bool)GetValue(CanLoadMoreProperty);
+            }
+            set
+            {
+                if (this.CanLoadMore != value)
+                {
+                    base.SetValue(CanLoadMoreProperty, value);
+
+                }
+
+            }
+        }
+
+        public static BindableProperty LoadMoreCommandProperty = BindableProperty.Create(
+                       "LoadMoreCommand",
+                       typeof(ICommand),
+                       typeof(ListView));
+        public ICommand LoadMoreCommand
+        {
+            get
+            {
+                return (ICommand)GetValue(LoadMoreCommandProperty);
+            }
+            set
+            {
+                if (LoadMoreCommand != value)
+                {
+                    SetValue(LoadMoreCommandProperty, value);
+                }
+            }
+        }
+
+        public ListViewInfiniteScroll() : base(GetCachingStrategy())
+        {
+            Initialize();
+
+        }
+
+        public ListViewInfiniteScroll(ListViewCachingStrategy cachingStrategy) : base(cachingStrategy)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            ItemAppearing += ListviewInfinite_ItemAppearing;
+        }
+
+        static ListViewCachingStrategy GetCachingStrategy()
+        {
+            switch (Device.RuntimePlatform)
+            {
+                case Device.Android:
+                    return ListViewCachingStrategy.RecycleElement;
+                default:
+                    return ListViewCachingStrategy.RetainElement;
+            }
+        }
+
+        private void ListviewInfinite_ItemAppearing(object sender, ItemVisibilityEventArgs e)
+        {
+            try
+            {
+                if (!CanLoadMore)
+                {
+                    return;
+                }
+
+                var items = ItemsSource as IList;
+
+                if (items != null && e.Item == items[items.Count - 1])
+                {   
+
+                    if (LoadMoreCommand != null)
+                    {
+                        LoadMoreCommand.Execute(null);                        
+                    }
+
+                 
+                }
+            }
+            catch (Exception ex)
+            {                
+                throw;
+            }
+
+        }
+    }
+}
---
+++ InfiniteListView/InfiniteListView/ViewModels/ProductViewModel.cs
@@ -0,0 +1,66 @@
+﻿using InfiniteListView.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace InfiniteListView.ViewModels
+{
+    public class ProductViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand LoadMoreCommand { get; set; }
+        public ObservableCollection<Product> Products { get; set; }
+        public bool CanLoadMore { get; set; }
+        private bool isBusy { get; set; }
+        public bool IsBusy { get { return isBusy; } set { isBusy = value; OnPropertyChanged(); } }
+        
+
+        public ProductViewModel()
+        {
+            LoadMoreCommand = new Command(async () => await LoadMore());            
+            InitializeProducts();
+            CanLoadMore = true;
+        }
+
+        private void InitializeProducts()
+        {
+            Products = new ObservableCollection<Product>();
+            for (int index = 1; index <= 25; index++)
+            {
+                Products.Add(new Product { Name = $"Product {index}" });
+            }
+
+        }
+
+        private async Task LoadMore()
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+
+            IsBusy = true;
+            CanLoadMore = false;
+
+            int nextPage = Products.Count + 5;
+
+            for (int index = Products.Count + 1; index < nextPage; index++)
+            {
+                Products.Add(new Product { Name = $"Product {index}" });
+                await Task.Delay(1500);
+            }
+
+            IsBusy = false;
+            CanLoadMore = true;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
